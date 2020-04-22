97fa463aaa6457cc0dcfd1e5b2da36c0dd725200
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-04-22 21:31:43 +0200

    Creamos nuevas constantes

--- app/src/main/java/com/jmonzon/minitwitter/utils/Constants.kt
+++ app/src/main/java/com/jmonzon/minitwitter/utils/Constants.kt
@@ -4,5 +4,10 @@ class Constants {
     companion object {
        const val baseUrl: String = "https://www.minitwitter.com:3001/apiv1/"
        const val tokenValue : String = "TOKEN_VALUE"
+       const val userName : String = "USERNAME_VALUE"
+       const val photoUrl : String = "PHOTOURL_VALUE"
+       const val email : String = "EMAIL_VALUE"
+       const val active : String = "ACTIVE_VALUE"
+       const val created : String = "CREATED_VALUE"
     }
 }
