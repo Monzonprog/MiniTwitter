7c30f8d6dabc922303744938ab4ec1e11da05b16
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-04-22 20:41:37 +0200

    Se crea método para poder compartir Context

---
+++ app/src/main/java/com/jmonzon/minitwitter/utils/MyApp.kt
@@ -0,0 +1,19 @@
+package com.jmonzon.minitwitter.utils
+
+import android.app.Application
+import android.content.Context
+
+abstract class MyApp : Application() {
+    companion object {
+
+        private lateinit var context: Context
+
+        fun setContext(con: Context) {
+            context = con
+        }
+
+        fun getContext() : Context{
+            return context
+        }
+    }
+}
