571dcde7251441044cd3a04b8dff91b06414b70f
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-04-22 20:42:11 +0200

    Refactorizamos cambiando nombre y añadimos constante para el string de token

--- app/src/main/java/com/jmonzon/minitwitter/utils/Constants.kt
+++ app/src/main/java/com/jmonzon/minitwitter/utils/Constants.kt
@@ -1,7 +1,8 @@
-package com.jmonzon.minitwitter.common
+package com.jmonzon.minitwitter.utils
 
 class Constants {
     companion object {
        const val baseUrl: String = "https://www.minitwitter.com:3001/apiv1/"
+       const val tokenValue : String = "TOKEN_VALUE"
     }
 }
