eeba139f4309ec676d3bc06e39ab88bb707808fd
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-04-22 21:32:34 +0200

    Creamos métodos para recuperar los datos guardados

--- app/src/main/java/com/jmonzon/minitwitter/utils/SharedPreferencesManager.kt
+++ app/src/main/java/com/jmonzon/minitwitter/utils/SharedPreferencesManager.kt
@@ -8,15 +8,30 @@ class SharedPreferencesManager {
     private var APP_SETTINGS_FILE: String = "APP_SETTINGS"
 
     private fun getSharedPreferences(): SharedPreferences {
-        return MyApp.Companion.getContext().getSharedPreferences(APP_SETTINGS_FILE, Context.MODE_PRIVATE)
+        return MyApp.getContext()
+            .getSharedPreferences(APP_SETTINGS_FILE, Context.MODE_PRIVATE)
     }
 
     fun setStringValueSharedPreferences(dataLabel: String, dataValue: String) {
-        with (getSharedPreferences().edit()) {
+        with(getSharedPreferences().edit()) {
             putString(dataLabel, dataValue)
             commit()
         }
+    }
 
+    fun setBooleanValueSharedPreferences(dataLabel: String, dataValue: Boolean) {
+        with(getSharedPreferences().edit()) {
+            putBoolean(dataLabel, dataValue)
+            commit()
+        }
+    }
+
+    fun getStringValueSharedPreferences(dataLabel: String): String {
+        return getSharedPreferences().getString(dataLabel, null)!!
+    }
+
+    fun getBooleanValueSharedPreferences(dataLabel: String): Boolean {
+        return getSharedPreferences().getBoolean(dataLabel, false)
     }
 
 }
