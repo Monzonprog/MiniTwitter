1da11e3b3ada98f8e52105f79be3cfc4f48a9ef1
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-04-24 20:11:05 +0200

    Refactorizamos y cabiamos de nombre

--- app/src/main/java/com/jmonzon/minitwitter/ui/DashboardActivity.kt
+++ app/src/main/java/com/jmonzon/minitwitter/ui/DashboardActivity.kt
@@ -22,7 +22,7 @@ class DashboardActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_tweet_like, R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
