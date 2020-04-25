de095ebbedea8c1e93448c09bc553f0383faef8d
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-04-25 12:53:26 +0200

    Creamos modelo para Tweet y para User

---
+++ app/src/main/java/com/jmonzon/minitwitter/models/Tweet.kt
@@ -0,0 +1,6 @@
+data class Tweet(
+    val id: Int,
+    val likes: List<Any>,
+    val mensaje: String,
+    val user: User
+)
---
+++ app/src/main/java/com/jmonzon/minitwitter/models/User.kt
@@ -0,0 +1,8 @@
+data class User(
+    val created: String,
+    val descripcion: String,
+    val id: Int,
+    val photoUrl: String,
+    val username: String,
+    val website: String
+)
