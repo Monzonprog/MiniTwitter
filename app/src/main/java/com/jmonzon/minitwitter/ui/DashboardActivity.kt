105b86ba5127e71c79fd8ede3fd0f3ce88ff4731
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-05-02 00:57:42 +0200

    Damos funcionanalidad al FAB para abrir la pantalla de nuevo tweet

--- app/src/main/java/com/jmonzon/minitwitter/ui/DashboardActivity.kt
+++ app/src/main/java/com/jmonzon/minitwitter/ui/DashboardActivity.kt
@@ -1,22 +1,28 @@
 package com.jmonzon.minitwitter.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jmonzon.minitwitter.R
+import com.jmonzon.minitwitter.ui.ui.NewTweetDialogFragment
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var fab : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
+        fab = findViewById(R.id.fab)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -27,5 +33,11 @@ class DashboardActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        fab.setOnClickListener(View.OnClickListener {
+            val dialogFragment : NewTweetDialogFragment = NewTweetDialogFragment()
+            dialogFragment.show(supportFragmentManager, "NewTweetDialogFragment")
+        })
     }
 }
