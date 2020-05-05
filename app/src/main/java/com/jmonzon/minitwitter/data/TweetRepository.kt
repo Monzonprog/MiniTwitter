3f1044221983f54e4c91a16265f7e1cde12c4529
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-05-05 20:15:11 +0200

    Creamos método el repositorio para crear nuevo tweet

--- app/src/main/java/com/jmonzon/minitwitter/data/TweetRepository.kt
+++ app/src/main/java/com/jmonzon/minitwitter/data/TweetRepository.kt
@@ -2,9 +2,9 @@ package com.jmonzon.minitwitter.data
 
 import android.util.Log
 import android.widget.Toast
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.jmonzon.minitwitter.common.MyApp
+import com.jmonzon.minitwitter.models.RequestCreateTweet
 import com.jmonzon.minitwitter.models.Tweet
 import com.jmonzon.minitwitter.retrofit.AuthMiniTwitterClient
 import com.jmonzon.minitwitter.retrofit.AuthMiniTwitterService
@@ -16,7 +16,7 @@ class TweetRepository {
 
     private lateinit var authMiniTwitterService: AuthMiniTwitterService
     private lateinit var authMiniTwitterClient: AuthMiniTwitterClient
-    private lateinit var allTweetsRecovered: LiveData<List<Tweet>>
+    private var allTweetsRecovered: MutableLiveData<List<Tweet>> = MutableLiveData()
 
     init {
         initRetrofit()
@@ -28,8 +28,7 @@ class TweetRepository {
         allTweetsRecovered = getAllTweets()
     }
 
-    fun getAllTweets(): LiveData<List<Tweet>> {
-        val data: MutableLiveData<List<Tweet>> = MutableLiveData()
+    fun getAllTweets(): MutableLiveData<List<Tweet>> {
 
         val call: Call<List<Tweet>> = authMiniTwitterService.getAllTweets()
         call.enqueue(object : Callback<List<Tweet>> {
@@ -38,7 +37,7 @@ class TweetRepository {
                 response: Response<List<Tweet>>
             ) {
                 if (response.isSuccessful) {
-                    data.value = response.body()
+                    allTweetsRecovered.value = response.body()
                     Log.i("TweetRepository", "Se han recibido los tweets")
                 } else {
                     Toast.makeText(
@@ -57,6 +56,45 @@ class TweetRepository {
                 ).show()
             }
         })
-        return data
+        return allTweetsRecovered
+    }
+
+    fun createTweet(message: String) {
+
+        val requestCreateTweet = RequestCreateTweet(message)
+
+        val call: Call<Tweet> = authMiniTwitterService.createTweet(requestCreateTweet)
+
+        call.enqueue(object : Callback<Tweet> {
+            override fun onResponse(
+                call: Call<Tweet>,
+                response: Response<Tweet>
+            ) {
+                if (response.isSuccessful) {
+                    //Add in first place created tweet
+                    val listAux: ArrayList<Tweet> = ArrayList()
+                    listAux.add(response.body()!!)
+                    for (tweet in allTweetsRecovered.value!!) {
+                        listAux.add(Tweet(tweet))
+                    }
+                    allTweetsRecovered.value = listAux
+                } else {
+                    Toast.makeText(
+                        MyApp.getContext(),
+                        "Algo ha ido mal, por favor intentalo más tarde",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Tweet>, t: Throwable) {
+                Toast.makeText(
+                    MyApp.getContext(),
+                    "Erro en la conexión, ha ocurrido un error al crear el tweet",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        })
     }
 }
