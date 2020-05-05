9de871744dc7a22c80415f0b9852ea900bc51c34
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-05-05 20:16:08 +0200

    Creamos método para crear tweet en el servicio

--- app/src/main/java/com/jmonzon/minitwitter/retrofit/AuthMiniTwitterService.kt
+++ app/src/main/java/com/jmonzon/minitwitter/retrofit/AuthMiniTwitterService.kt
@@ -1,11 +1,17 @@
 package com.jmonzon.minitwitter.retrofit
 
+import com.jmonzon.minitwitter.models.RequestCreateTweet
 import com.jmonzon.minitwitter.models.Tweet
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface AuthMiniTwitterService {
 
     @GET("tweets/all")
     fun getAllTweets(): Call<List<Tweet>>
+
+    @POST("tweets/create")
+    fun createTweet(@Body requestCreateTweet: RequestCreateTweet): Call<Tweet>
 }
