873d3f0fd3a26364012d31452e37b0497476f5ac
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-05-08 10:00:11 +0200

    Añadimos comentarios

--- app/src/main/java/com/jmonzon/minitwitter/common/Constants.kt
+++ app/src/main/java/com/jmonzon/minitwitter/common/Constants.kt
@@ -2,13 +2,17 @@ package com.jmonzon.minitwitter.common
 
 class Constants {
     companion object {
+        //URL´s
        const val baseUrl: String = "https://www.minitwitter.com:3001/apiv1/"
        const val baseUrlPhotos: String = "https://www.minitwitter.com/apiv1/uploads/photos/"
+
+        //Preferences
        const val tokenValue : String = "TOKEN_VALUE"
        const val userName : String = "USERNAME_VALUE"
        const val photoUrl : String = "PHOTOURL_VALUE"
        const val email : String = "EMAIL_VALUE"
        const val active : String = "ACTIVE_VALUE"
        const val created : String = "CREATED_VALUE"
+
     }
 }
