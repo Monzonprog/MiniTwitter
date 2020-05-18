635f0f6be4ec5c2677b4fd3a8a0a82593574153e
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-05-18 23:54:02 +0200

    Creamos Fragment del perfil de usuario

--- app/src/main/java/com/jmonzon/minitwitter/ui/dashboard/profile/ProfileFragment.kt
+++ app/src/main/java/com/jmonzon/minitwitter/ui/dashboard/profile/ProfileFragment.kt
@@ -4,28 +4,70 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jmonzon.minitwitter.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var ivAvatar: CircleImageView
+    private lateinit var etUsername: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etCurrentPassword: EditText
+    private lateinit var btSave: Button
+    private lateinit var btChangePassword: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = "Profile"
-        })
-        return root
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        //Instanciate FAB from dashboard activity
+        val fab: FloatingActionButton = requireActivity().findViewById(R.id.fab)
+        //Hide FAB in this screen
+        fab.hide()
+
+        findViews(view)
+        createEvents()
+
+
+
+        return view
+    }
+
+    private fun findViews(view: View) {
+        ivAvatar = view.findViewById(R.id.imageViewAvatar)
+        etUsername = view.findViewById(R.id.editTextUserName)
+        etEmail = view.findViewById(R.id.editTextEmail)
+        etCurrentPassword = view.findViewById(R.id.editTextCurrentPassword)
+        btSave = view.findViewById(R.id.buttonSave)
+        btChangePassword = view.findViewById(R.id.buttonChangePassword)
+    }
+
+    private fun createEvents() {
+        btSave.setOnClickListener(listener)
+        btChangePassword.setOnClickListener(listener)
+    }
+
+    private val listener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.buttonSave -> {
+                Toast.makeText(context, "Botón save", Toast.LENGTH_SHORT).show()
+            }
+            R.id.buttonChangePassword -> {
+                Toast.makeText(context, "Botón cambiar clave", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
