3609ef6658d28cefcdb6e781d0925db235f53466
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-05-23 19:18:14 +0200

    Creamos método en el viewModel para recuperar información de usuario

--- app/src/main/java/com/jmonzon/minitwitter/ui/dashboard/profile/ProfileViewModel.kt
+++ app/src/main/java/com/jmonzon/minitwitter/ui/dashboard/profile/ProfileViewModel.kt
@@ -1,9 +1,18 @@
 package com.jmonzon.minitwitter.ui.dashboard.profile
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jmonzon.minitwitter.data.ProfileRepository
+import com.jmonzon.minitwitter.models.ResponseUserProfile
 
 class ProfileViewModel : ViewModel() {
 
+    companion object {
+        private val profileRepository: ProfileRepository = ProfileRepository()
+        var profile: LiveData<ResponseUserProfile> = profileRepository.getProfile()
+    }
+
+    fun getProfile(): LiveData<ResponseUserProfile>{
+        return profile
+    }
 }
