89d5980e16a521dd3ab081712c6dbbd709db6f0d
Jorge Monzón <jorge.monzon.manzano@everis.com>
2020-05-24 20:09:33 +0200

    Eliminamos cache de imagenes, añadimos nuevos campos, comprobamos los campos antes de enviar petición

--- app/src/main/java/com/jmonzon/minitwitter/ui/dashboard/profile/ProfileFragment.kt
+++ app/src/main/java/com/jmonzon/minitwitter/ui/dashboard/profile/ProfileFragment.kt
@@ -11,9 +11,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jmonzon.minitwitter.R
 import com.jmonzon.minitwitter.common.Constants
+import com.jmonzon.minitwitter.models.RequestUserProfile
 import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileFragment : Fragment() {
@@ -27,6 +29,7 @@ class ProfileFragment : Fragment() {
     private lateinit var etDescription: EditText
     private lateinit var btSave: Button
     private lateinit var btChangePassword: Button
+    var loadingData: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,7 +44,6 @@ class ProfileFragment : Fragment() {
         val fab: FloatingActionButton = requireActivity().findViewById(R.id.fab)
         //Hide FAB in this screen
         fab.hide()
-
         findViews(view)
         createEvents()
 
@@ -54,7 +56,16 @@ class ProfileFragment : Fragment() {
             if (it.photoUrl != "") {
                 Glide.with(this)
                     .load(Constants.baseUrlPhotos + it.photoUrl)
+                    .dontAnimate()
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .centerCrop()
                     .into(ivAvatar)
+            }
+            if(!loadingData) {
+                Toast.makeText(context, "Datos guardados correctamente", Toast.LENGTH_SHORT)
+                    .show()
+                btSave.isEnabled = true
             }
         })
 
@@ -66,26 +77,54 @@ class ProfileFragment : Fragment() {
         etUsername = view.findViewById(R.id.editTextUserName)
         etEmail = view.findViewById(R.id.editTextEmail)
         etCurrentPassword = view.findViewById(R.id.editTextCurrentPassword)
-        btSave = view.findViewById(R.id.buttonSave)
-        btChangePassword = view.findViewById(R.id.buttonChangePassword)
         etWebsite = view.findViewById(R.id.editTextWebSite)
         etDescription = view.findViewById(R.id.editTextDescription)
+        btSave = view.findViewById(R.id.buttonSave)
+        btChangePassword = view.findViewById(R.id.buttonChangePassword)
     }
 
     private fun createEvents() {
         btSave.setOnClickListener(listener)
         btChangePassword.setOnClickListener(listener)
+        ivAvatar.setOnClickListener(listener)
     }
 
     private val listener = View.OnClickListener { view ->
         when (view.id) {
             R.id.buttonSave -> {
-                Toast.makeText(context, "Botón save", Toast.LENGTH_SHORT).show()
+                when {
+                    etUsername.text.toString().isEmpty() -> {
+                        etUsername.error = "El nombre de usuario es necesario"
+                    }
+                    etEmail.text.toString().isEmpty() -> {
+                        etEmail.error = "El email es necesario"
+                    }
+                    etCurrentPassword.text.toString().isEmpty() -> {
+                        etCurrentPassword.error = "La contraseña es requerida"
+                    }
+                    else -> {
+                        val requestUserProfile = RequestUserProfile(
+                            etDescription.text.toString(),
+                            etEmail.text.toString(),
+                            etCurrentPassword.text.toString(),
+                            etUsername.text.toString(),
+                            etWebsite.text.toString()
+                        )
+                        profileViewModel.updateProfile(requestUserProfile)
+                        Toast.makeText(context, "Enviando información al servidor", Toast.LENGTH_SHORT).show()
+                        btSave.isEnabled = false
+                        loadingData = false
+                    }
+                }
             }
             R.id.buttonChangePassword -> {
                 Toast.makeText(context, "Botón cambiar clave", Toast.LENGTH_SHORT).show()
+            }
+            R.id.imageViewAvatar -> {
+                Toast.makeText(context, "Se seleccionará la foto", Toast.LENGTH_SHORT).show()
 
             }
         }
     }
 }
+
